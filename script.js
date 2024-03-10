2128a4da9d682ffdace9b29838acb48df5b7f0a7
kaialiani <111850398+kaialiani@users.noreply.github.com>
2024-03-10 21:23:32 +0100

    updated social media links and mobile view

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-const carouselInner = document.querySelector(".carousel-inner");
-const prevArrow = document.querySelector(".prev-arrow");
-const nextArrow = document.querySelector(".next-arrow");
-const itemWidth = carouselInner.querySelector(".carousel-item").offsetWidth;
-let currentTranslate = 0;
-
-const slide = (direction) => {
-  currentTranslate += itemWidth * 3 * direction;
-  carouselInner.style.transform = `translateX(${currentTranslate}px)`;
-};
-
-prevArrow.addEventListener("click", () => {
-  slide(-1);
-});
-
-nextArrow.addEventListener("click", () => {
-  slide(1);
+document.addEventListener("DOMContentLoaded", function () {
+  var fadeInContainer = document.querySelector(".fade-in-container");
+  fadeInContainer.classList.add("fade-in");
 });
